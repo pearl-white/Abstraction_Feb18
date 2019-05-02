1816070f51d7e5a43f23d43c56b8193c15971781
Guga <Guga@Gugas-MacBook-Air.local>
2019-05-01 22:55:12 -0500

    first commit

--- src/abstraction_Feb21/Car.java
+++ src/abstraction_Feb21/Car.java
@@ -3,7 +3,7 @@ package abstraction_Feb21;
 public class Car extends Vehicle {
 
 	public void start() {
-		System.out.println("Starting"); // hdshgf
+		System.out.println("Starting"); // line...
 	}
 
 	public void accelerate(int mph) {
